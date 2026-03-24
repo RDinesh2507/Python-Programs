72b2389f9d574f27b5b9f971c6132d0882fa8060
Dinesh Ranganathan <rdinesh250704@gmail.com>
2026-03-24 21:18:36 +0530

    Add files via upload

---
+++ 810.py
@@ -0,0 +1,6 @@
+from functools import reduce
+from operator import xor
+from typing import List
+class Solution:
+    def xorGame(self, nums: List[int]) -> bool:
+        return len(nums)%2==0 or reduce(xor,nums)==0
